656eab20572089b14bc9397049802dff5f21651f
Logan Buchanan <Loganbuchanan@hotmail.com>
2018-11-28 15:57:16 -0800

    Added a now playing interface
    All of it is synced together so any play button activate it

--- public_html/misc.js
+++ public_html/misc.js
@@ -3,16 +3,19 @@ console.log("Running...")
 var fav = document.getElementById("favorite");
 var bought = document.getElementById("buyalbum");
 var play = document.getElementById("playbutt");
+var playblack = document.getElementById("playbuttblack");
 
 console.log("Fav =" + fav);
 fav.addEventListener("click", changefav);
 bought.addEventListener("click", changebuy);
 play.addEventListener("click", changeplay);
+playblack.addEventListener("click", changeplay);
 
 var filled = false;
 var yougotit = false;
 var itsplayingalright = false;
 
+//Slicing arrays solution found from bottens' answer here: https://stackoverflow.com/questions/24639493/gather-divs-and-perform-css-on-them-htmlcollection-vs-array
 var soundbars = document.getElementsByClassName("soundbar");
 soundbars = Array.prototype.slice.call(soundbars);
 
@@ -30,9 +33,12 @@ function changefav(){
 function changeplay(){
     if(!itsplayingalright){
         play.src = "img/pause.png";
+        playblack.src = "img/pause-black.png";
         itsplayingalright = true;
+        
     }else{
         play.src = "img/play-arrow.png";
+        playblack.src = "img/play-arrow-black.png";
         itsplayingalright = false;
         soundbars.forEach( function(element){
             
@@ -53,19 +59,12 @@ function changebuy(){
 
 }
 
-var soundbars = document.getElementsByClassName("soundbar");
-soundbars = Array.prototype.slice.call(soundbars);
-
 
 //Set interval grabbed from Doug Neiner's answer here: https://stackoverflow.com/questions/2170923/whats-the-easiest-way-to-call-a-function-every-5-seconds-in-jquery
 window.setInterval(function(){
     
-
-    
-
     if(itsplayingalright){
         soundbars.forEach( function(element){
-            
             element.style.height = Math.floor(Math.random() * 100) + "%"; 
         });
     }
