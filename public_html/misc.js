e07e9e25899b4fce140b9b4a521e0a320c05e72b
Logan Buchanan <Loganbuchanan@hotmail.com>
2018-11-28 15:40:38 -0800

    A Waveform!?
    Got a gosh darn waveform on dat page
    (it's fake)

--- public_html/misc.js
+++ public_html/misc.js
@@ -13,6 +13,9 @@ var filled = false;
 var yougotit = false;
 var itsplayingalright = false;
 
+var soundbars = document.getElementsByClassName("soundbar");
+soundbars = Array.prototype.slice.call(soundbars);
+
 function changefav(){
     console.log("Clicked!");
     if(!filled){
@@ -31,6 +34,10 @@ function changeplay(){
     }else{
         play.src = "img/play-arrow.png";
         itsplayingalright = false;
+        soundbars.forEach( function(element){
+            
+            element.style.height = "10%"; 
+        });
     }
 }
 
@@ -45,3 +52,22 @@ function changebuy(){
     }
 
 }
+
+var soundbars = document.getElementsByClassName("soundbar");
+soundbars = Array.prototype.slice.call(soundbars);
+
+
+//Set interval grabbed from Doug Neiner's answer here: https://stackoverflow.com/questions/2170923/whats-the-easiest-way-to-call-a-function-every-5-seconds-in-jquery
+window.setInterval(function(){
+    
+
+    
+
+    if(itsplayingalright){
+        soundbars.forEach( function(element){
+            
+            element.style.height = Math.floor(Math.random() * 100) + "%"; 
+        });
+    }
+    
+}, 100)
